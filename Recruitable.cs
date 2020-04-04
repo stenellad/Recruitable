4c1c8f69c0ae5217d89425ec8e9922e5e0d6ab75
Stenella <35588191+stenellad@users.noreply.github.com>
2020-04-03 20:14:08 -0700

    Initial commit

---
+++ Recruitable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Core;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+
+namespace Recruitable
+{
+    public class Recruitable : DefaultPrisonerRecruitmentCalculationModel
+    {
+        public override float[] GetDailyRecruitedPrisoners(MobileParty mainParty)
+        {
+            return new float[]
+            {
+                1f,
+                0.7f,
+                0.5f,
+                0.4f,
+                0.3f,
+                0.2f,
+                0.1f
+            };
+        }
+    }
+}
---
+++ RecruitableSubModule.cs
@@ -0,0 +1,16 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace Recruitable
+{
+    public class RecruitableSubModule : MBSubModuleBase
+    {
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            if (!(game.GameType is Campaign))
+                return;
+            gameStarterObject.AddModel(new Recruitable());
+        }
+    }
+}
